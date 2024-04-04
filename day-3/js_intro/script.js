4423167b520963bac4c451e3f25e8217d15b714a
anand dhar dwivedi <ananddhardwivedi05@gmail.com>
2024-04-04 15:23:04 +0530

    day-3

---
+++ day-3/js_intro/script.js
@@ -0,0 +1,24 @@
+// console.log("hello javaScript");
+
+// var number = 90;
+// var num2;
+// function car() {
+//   num2 = 29;
+//   console.log(number);
+//   console.log(num2);
+//   console.log(num2);
+// }
+
+// car();
+
+// console.log(number);
+// console.log(num2);
+// // var is function scope
+// alert("hi");
+
+let str = "12";
+let ans = 20;
+
+let sum = str * 1 - 20;
+
+console.log(sum);
