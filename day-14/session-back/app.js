f5a45a02510a75a5b6bcb186177bc7c85ed265cb
anand dhar dwivedi <ananddhardwivedi05@gmail.com>
2024-06-03 00:47:14 +0530

    session-front

--- day-14/session-back/app.js
+++ day-14/session-back/app.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:1234");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:1234");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://image-genration.vercel.app/"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
