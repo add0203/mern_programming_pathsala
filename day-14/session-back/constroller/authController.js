a2a72b7b8c1a7a77443c6323beaf4c14566195cc
anand dhar dwivedi <ananddhardwivedi05@gmail.com>
2024-06-05 01:22:13 +0530

    SignIn controller

--- day-14/session-back/constroller/authController.js
+++ day-14/session-back/constroller/authController.js
@@ -1,9 +1,23 @@
 const userModel = require("../models/authModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const salt = bcrypt.genSaltSync(10);
+const passwordHash = async (password) => {
+  return await bcrypt.hash(password, salt);
+};
+
+const genrateToken = async (userId) => {
+  return await jwt.sign({ userId: userId }, "process.env.JWT_SECRET", {
+    expiresIn: "90d",
+  });
+};
 
 const signUp = async (req, res) => {
   const { email: email, password: password } = req.body;
 
-  const hashPassword = () => {};
+  console.log(password);
+  console.log(email);
   try {
     if (!email || !password) {
       res.json({
@@ -14,23 +28,25 @@ const signUp = async (req, res) => {
       });
     }
 
+    const hashedPassword = await passwordHash(password);
+
     const userCreated = await userModel.create({
       email,
-      password,
+      password: hashedPassword,
     });
 
-    res.json({
-      status: 200,
+    res.status(200).json({
       user: {
         userData: userCreated,
+        // it may be _id : ERROR
+        Token: await genrateToken(userCreated._id),
       },
     });
   } catch (error) {
-    res.json({
-      status: 401,
-      Error: {
-        error: error,
-      },
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: error?.message,
     });
   }
 };
@@ -38,27 +54,49 @@ const signUp = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.json({
-      status: 401,
-      userData: {
-        user: "not Created",
-      },
-    });
-
-    const user = userModel.findOne({ email });
-
-    if (!user) {
+  try {
+    if (!email || !password) {
       res.json({
         status: 401,
-        user: {
-          error: "user NOT EXIST",
+        userData: {
+          user: "not Created",
         },
       });
+    } else {
+      const userExists = await userModel.findOne({ email });
+
+      if (!userExists) {
+        res.status(404).json({
+          status: "user not exist",
+        });
+      } else {
+        const hashedPassword = userExists.password;
+        const result = await bcrypt.compare(password, hashedPassword);
+
+        if (!result) {
+          res.status(401).json({
+            status: "Unauthorized",
+          });
+        } else {
+          res.status(201).json({
+            status: "success",
+            data: {
+              user: userExists,
+              token: await genrateToken(userExists.id),
+            },
+          });
+        }
+      }
     }
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error?.message,
+    });
   }
 };
 
 module.exports = {
   signUp,
+  login,
 };
--- day-14/session-back/routes/authRouter.js
+++ day-14/session-back/routes/authRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signUp } = require("../constroller/authController");
+const { signUp, login } = require("../constroller/authController");
 const router = express.Router();
 
 // Route get image
 router.route("/signUp").post(signUp);
+router.route("/logIn").post(login);
 
 module.exports = router;
