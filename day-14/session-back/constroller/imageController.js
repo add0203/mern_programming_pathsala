4d502b8a7c41d6bd45d5bbef46f5c8e2f99719e2
Anand Dhar Dwivedi <120220077+add0203@users.noreply.github.com>
2024-06-26 14:27:32 +0530

    imageController.js

--- day-14/session-back/constroller/imageController.js
+++ day-14/session-back/constroller/imageController.js
@@ -139,29 +139,50 @@ const generateImage = async (req, res) => {
   let imageUrl = "";
   try {
     const res = await fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
-      headers: {
-        accept: "*/*",
-        "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-        "api-token":
-          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTgxNTk5OTUsImV4cCI6MTcxODE2MzU5NX0.y-92ajIZsohPzcyLVlqyWM2hM9-6I2rgdtnTv_64ntI",
-        authorization: "hotpot-t2mJbCr8292aQzp8CnEPaK",
-        "content-type":
-          "multipart/form-data; boundary=----WebKitFormBoundaryACOWWzhDfQIO35NN",
-        "sec-ch-ua":
-          '"Not/A)Brand";v="8", "Chromium";v="126", "Microsoft Edge";v="126"',
-        "sec-ch-ua-mobile": "?1",
-        "sec-ch-ua-platform": '"Android"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-      },
-      referrer: "https://hotpot.ai/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: `------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="seedValue"\r\n\r\nnull\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="inputText"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="width"\r\n\r\n512\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="height"\r\n\r\n512\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="styleId"\r\n\r\n146\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="styleLabel"\r\n\r\nConcept Art 7\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="isPrivate"\r\n\r\nfalse\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="price"\r\n\r\n0\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="requestId"\r\n\r\n22-iHSULSReKmXwykk\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="resultUrl"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/22-iHSULSReKmXwykk.png\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN--\r\n`,
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-    });
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
+    "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTkzOTIwNzQsImV4cCI6MTcxOTM5OTI3NH0.FsBA1lfDALTsqZrG98KbInspAD7d7I6P7M1FpVn6TKQ",
+    "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryYX9uwQ0h2Rpg2oFs",
+    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://hotpot.ai/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\ncar\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n146\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nConcept Art 7\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n22-IXdGbWchs2plTq9\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/22-IXdGbWchs2plTq9.png\r\n------WebKitFormBoundaryYX9uwQ0h2Rpg2oFs--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+    //   fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
+    //   headers: {
+    //     accept: "*/*",
+    //     "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+    //     "api-token":
+    //       "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTgxNTk5OTUsImV4cCI6MTcxODE2MzU5NX0.y-92ajIZsohPzcyLVlqyWM2hM9-6I2rgdtnTv_64ntI",
+    //     authorization: "hotpot-t2mJbCr8292aQzp8CnEPaK",
+    //     "content-type":
+    //       "multipart/form-data; boundary=----WebKitFormBoundaryACOWWzhDfQIO35NN",
+    //     "sec-ch-ua":
+    //       '"Not/A)Brand";v="8", "Chromium";v="126", "Microsoft Edge";v="126"',
+    //     "sec-ch-ua-mobile": "?1",
+    //     "sec-ch-ua-platform": '"Android"',
+    //     "sec-fetch-dest": "empty",
+    //     "sec-fetch-mode": "cors",
+    //     "sec-fetch-site": "same-site",
+    //   },
+    //   referrer: "https://hotpot.ai/",
+    //   referrerPolicy: "strict-origin-when-cross-origin",
+    //   body: `------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="seedValue"\r\n\r\nnull\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="inputText"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="width"\r\n\r\n512\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="height"\r\n\r\n512\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="styleId"\r\n\r\n146\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="styleLabel"\r\n\r\nConcept Art 7\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="isPrivate"\r\n\r\nfalse\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="price"\r\n\r\n0\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="requestId"\r\n\r\n22-iHSULSReKmXwykk\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN\r\nContent-Disposition: form-data; name="resultUrl"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/22-iHSULSReKmXwykk.png\r\n------WebKitFormBoundaryACOWWzhDfQIO35NN--\r\n`,
+    //   method: "POST",
+    //   mode: "cors",
+    //   credentials: "include",
+    // });
     imageUrl = await res.json();
     let imageUrlForMongodb = await downloadAndUpload(imageUrl, searchText);
     imageCloudURL = imageUrlForMongodb;
