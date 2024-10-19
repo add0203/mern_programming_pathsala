a5ae485dd3ba9c93ce2f49bff8d0abb585c469cf
Anand Dhar Dwivedi <120220077+add0203@users.noreply.github.com>
2024-10-19 20:45:27 +0530

    Update imageController.js

--- day-14/session-back/constroller/imageController.js
+++ day-14/session-back/constroller/imageController.js
@@ -142,21 +142,44 @@ const generateImage = async (req, res) => {
   "headers": {
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-    "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mjg2NDQzMTcsImV4cCI6MTcyODY1MTUxN30.a26Y2qtG7kinqU-FaJCCdzGaW8588QHvrOVI_9BQHoo",
+    "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjkzNTA4NjIsImV4cCI6MTcyOTM1ODA2Mn0.kYAedY95vVmlgCSt4KAzobASRj4DBEhWRstQ4rmSvFk",
     "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
-    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryxKNU8LidskAlQ7A7",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryChIsKeYpruUYOP4b",
     "sec-ch-ua": "\"Chromium\";v=\"130\", \"Microsoft Edge\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
     "sec-ch-ua-mobile": "?1",
     "sec-ch-ua-platform": "\"Android\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "Referer": "https://hotpot.ai/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
+    "sec-fetch-site": "same-site"
   },
-  "body": `------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-iXc1WSectgiqlks\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-iXc1WSectgiqlks.png\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7--\r\n`,
-  "method": "POST"
+  "referrer": "https://hotpot.ai/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-bDTReLvj5oqU8nW\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-bDTReLvj5oqU8nW.png\r\n------WebKitFormBoundaryChIsKeYpruUYOP4b--\r\n",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
 });
+      
+      
+//       fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
+//   "headers": {
+//     "accept": "*/*",
+//     "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+//     "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mjg2NDQzMTcsImV4cCI6MTcyODY1MTUxN30.a26Y2qtG7kinqU-FaJCCdzGaW8588QHvrOVI_9BQHoo",
+//     "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
+//     "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryxKNU8LidskAlQ7A7",
+//     "sec-ch-ua": "\"Chromium\";v=\"130\", \"Microsoft Edge\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+//     "sec-ch-ua-mobile": "?1",
+//     "sec-ch-ua-platform": "\"Android\"",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-site",
+//     "Referer": "https://hotpot.ai/",
+//     "Referrer-Policy": "strict-origin-when-cross-origin"
+//   },
+//   "body": `------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-iXc1WSectgiqlks\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-iXc1WSectgiqlks.png\r\n------WebKitFormBoundaryxKNU8LidskAlQ7A7--\r\n`,
+//   "method": "POST"
+// });
 
 //    fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
 //   "headers": {
